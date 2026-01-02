f9f78a7aec3cd51e1dc2281ba9c6ac226b665d36
Haoming Xia <hewlett-packard-lovecraft@github.com>
2026-01-01 20:23:04 -0800

    jan 01 ws-t.el

---
+++ wsl-t.el
@@ -0,0 +1,22 @@
+;;; wsl-t.el  -*- lexical-binding: t; -*-
+;; wsl and terminal specific options
+
+;; copy and paste from clipboard
+(defun wsl-p ()
+  "Return t if Emacs is running in WSL2, nil otherwise."
+  (and (eq system-type 'gnu/linux)
+       (getenv "WSLENV")))
+
+(when (and (wsl-p)
+	   (eq (display-graphic-p) nil))
+
+  ;; https://www.rahuljuliato.com/posts/emacs-clipboard-terminal
+  (setq interprogram-cut-function
+	(lambda (text &optional _)
+	  (let ((process-connection-type nil))
+	    (let ((proc (start-process "clip.exe" "*Messages*" "clip.exe")))
+	      (process-send-string proc text)
+	      (process-send-eof proc)))))
+  (setq interprogram-paste-function
+	(lambda ()
+	  (string-trim (shell-command-to-string "powershell.exe -command Get-Clipboard")))))
